afc03d4586ba83addef59fe45b57fb53e4d80df8
LPMatrix <mubaraqsanusi908@gmail.com>
2020-01-06 18:00:54 +0100

    pass

--- admin/server/conn.php
+++ admin/server/conn.php
@@ -7,7 +7,7 @@
 // $mysqli = new mysqli($db_host, $db_username, $db_password,$db_name);
 
 $db_username = 'root';
-$db_password = 'Islam4you@2019';
+$db_password = '';
 $db_name = 'creditalliance';
 $db_host = 'localhost';
 $mysqli = new mysqli($db_host, $db_username, $db_password,$db_name);
